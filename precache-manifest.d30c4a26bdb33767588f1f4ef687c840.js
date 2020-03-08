f432d507a5d0fd0380eb03d602282f504451bcce
unknown <mummidi.raemsh@gmail.com>
2020-03-08 08:51:25 +0530

    Updates

---
+++ precache-manifest.d30c4a26bdb33767588f1f4ef687c840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e3312dfb429598e53bbd71906b9841",
+    "url": "/starwarstarships/index.html"
+  },
+  {
+    "revision": "29afec68eaddf1276af1",
+    "url": "/starwarstarships/static/css/main.21bc7d61.chunk.css"
+  },
+  {
+    "revision": "4ebf376760e2ef1d0e7a",
+    "url": "/starwarstarships/static/js/2.f075ddaf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/starwarstarships/static/js/2.f075ddaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29afec68eaddf1276af1",
+    "url": "/starwarstarships/static/js/main.1a340bda.chunk.js"
+  },
+  {
+    "revision": "a269dd5669606cdf3171",
+    "url": "/starwarstarships/static/js/runtime-main.52f0aafa.js"
+  }
+]);
